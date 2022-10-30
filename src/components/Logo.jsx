77576fe69cca478072506a327a05c852b3b63eed
xtrp <36894883+xtrp@users.noreply.github.com>
2022-10-30 17:31:59 -0400

    feat: updates

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,6 +2,15 @@ import Image from 'next/future/image'
 import LogoImage from '@/images/logo-small.png'
 export function Logo(props) {
   return (
-    <Image src={LogoImage} alt="" width={200} priority optimized {...props} />
+    <a href="/">
+      <Image
+        src={LogoImage}
+        alt=""
+        width={200}
+        priority
+        unoptimized
+        {...props}
+      />
+    </a>
   )
 }
