b8768fb774bd99c885eae35e1a778e49a61b926f
xtrp <36894883+xtrp@users.noreply.github.com>
2022-10-31 10:49:38 -0400

    feat: remove border from team

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -87,25 +87,7 @@ const team = [
   },
 ]
 
-function ImageClipPaths({ id, ...props }) {
-  return (
-    <svg aria-hidden="true" width={0} height={0} {...props}>
-      <defs>
-        <clipPath id={`${id}-0`} clipPathUnits="objectBoundingBox">
-          <path d="M0,0 h0.729 v0.129 h0.121 l-0.016,0.032 C0.815,0.198,0.843,0.243,0.885,0.243 H1 v0.757 H0.271 v-0.086 l-0.121,0.057 v-0.214 c0,-0.032,-0.026,-0.057,-0.057,-0.057 H0 V0" />
-        </clipPath>
-        <clipPath id={`${id}-1`} clipPathUnits="objectBoundingBox">
-          <path d="M1,1 H0.271 v-0.129 H0.15 l0.016,-0.032 C0.185,0.802,0.157,0.757,0.115,0.757 H0 V0 h0.729 v0.086 l0.121,-0.057 v0.214 c0,0.032,0.026,0.057,0.057,0.057 h0.093 v0.7" />
-        </clipPath>
-        <clipPath id={`${id}-2`} clipPathUnits="objectBoundingBox">
-          <path d="M1,0 H0.271 v0.129 H0.15 l0.016,0.032 C0.185,0.198,0.157,0.243,0.115,0.243 H0 v0.757 h0.729 v-0.086 l0.121,0.057 v-0.214 c0,-0.032,0.026,-0.057,0.057,-0.057 h0.093 V0" />
-        </clipPath>
-      </defs>
-    </svg>
-  )
-}
-
-export function Team({ signedIn = false }) {
+export function Team({ signedIn = false, fund = false }) {
   let id = useId()
 
   const roleClassnames = {
@@ -155,9 +137,10 @@ export function Team({ signedIn = false }) {
   return (
     <section
       id={signedIn ? 'explore' : 'team'}
-      className={signedIn ? 'pt-24 pb-20' : 'border-t py-20 sm:py-32'}
+      className={`${signedIn ? 'pt-24 pb-20' : 'py-20 sm:py-32'} ${
+        fund ? 'border-t' : ''
+      }`}
     >
-      {/* <ImageClipPaths id={id} /> */}
       <Container>
         <div className="mx-auto max-w-2xl lg:mx-0">
           <h2 className="font-display text-4xl font-medium tracking-tighter text-gray-800 sm:text-5xl">
